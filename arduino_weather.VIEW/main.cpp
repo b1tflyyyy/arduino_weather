f94b7cd574d5da3847bd6aed1f1b5f50b0737c29
b1tfly <thisisandrew0110@gmail.com>
2023-05-10 23:35:07 +0300

    added test get of temperature and humidity

--- arduino_weather.VIEW/main.cpp
+++ arduino_weather.VIEW/main.cpp
@@ -1,8 +1,15 @@
+#include "../arduino_weather.BL/Controller/WeatherController/WeatherController.h"
+#include "../arduino_weather.BL/Model/Weather/Weather.h"
+
 #include <iostream>
 
 int main()
 {
-    std::cout << "hello world!\n";
+    WeatherController weatherController;
+    Weather weather = weatherController.get_weather("Kropyvnytskyi");
 
+    std::cout << "current temp in " << weather.get_city() << " " << weather.get_temperature() << "C" << '\n';
+    std::cout << "current humidity in " << weather.get_city() << " " << weather.get_humidity() << "%" << '\n';
+    
     return 0;
 }

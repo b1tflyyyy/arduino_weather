bf6a339fc45090c0d7700a24f874b868fc8c970a
b1tfly <thisisandrew0110@gmail.com>
2023-05-10 23:30:11 +0300

    added WeatherController

---
+++ arduino_weather.BL/Controller/WeatherController/WeatherController.cpp
@@ -0,0 +1,45 @@
+#include "WeatherController.h"
+
+size_t WeatherController::m_handle_response(char* ptr,
+                                            size_t size,
+                                            size_t nmemb,
+                                            void* data)
+{
+    boost::property_tree::ptree* result = static_cast<boost::property_tree::ptree*>(data);
+
+    std::stringstream ss;
+    ss.write(ptr, size * nmemb);
+
+    boost::property_tree::read_json(ss, *result);
+    return size * nmemb;
+}
+
+Weather WeatherController::get_weather(const std::string& city)
+{
+    m_weather_url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",ua&units=metric&appid=" + m_api_key.get_api_key();
+
+    m_curl = curl_easy_init();
+
+    if (m_curl)
+    {
+        curl_easy_setopt(m_curl, CURLOPT_URL, m_weather_url.c_str());
+        curl_easy_setopt(m_curl, CURLOPT_WRITEFUNCTION, &WeatherController::m_handle_response);
+
+        boost::property_tree::ptree response;
+        curl_easy_setopt(m_curl, CURLOPT_WRITEDATA, &response);
+
+        CURLcode code = curl_easy_perform(m_curl);
+        curl_easy_cleanup(m_curl);
+
+        if (code == CURLE_OK)
+        {
+            float temperature = response.get<float>("main.temp");
+            float humidity = response.get<float>("main.humidity");
+
+            Weather current_weather(temperature, humidity, city);
+            return current_weather;
+        }
+    }
+
+    return Weather(0.0f, 0.0f, " ");
+}
---
+++ arduino_weather.BL/Controller/WeatherController/WeatherController.h
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef _WEATHER_CONTROLLER_H_
+#define _WEATHER_CONTROLLER_H_
+
+#define CURL_STATICLIB
+
+#include "../../../x64_curl/curl.h"
+
+#ifdef _DEBUG
+#pragma comment (lib, "../x64_curl/libcurl_a_debug.lib")
+#else
+#pragma comment (lib, "../x64_curl/libcurl_a.lib")
+#endif
+
+#pragma comment (lib, "Normaliz.lib")
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Wldap32.lib")
+#pragma comment (lib, "Crypt32.lib")
+#pragma comment (lib, "advapi32.lib")
+
+#include <string>
+#include <sstream>
+
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+#include "../arduino_weather.BL/Model/Weather/Weather.h"
+#include "../arduino_weather.BL/Model/APIKey/APIKey.h"
+
+
+class WeatherController
+{
+public:
+    WeatherController() = default;
+
+    ~WeatherController() = default;
+
+    Weather get_weather(const std::string& city);
+
+private:
+    static size_t m_handle_response(char* ptr,
+                                    size_t size,
+                                    size_t nmemb,
+                                    void* data);
+
+    CURL* m_curl;
+    APIKey m_api_key;
+    std::string m_weather_url;
+};
+
+
+#endif /* _WEATHER_CONTROLLER_H_ */
+

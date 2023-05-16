7f2ccfdb4f39849fd81825bae3f0e7dd53028031
b1tfly <thisisandrew0110@gmail.com>
2023-05-16 21:46:02 +0300

    beta v0.1

--- arduino_weather.VIEW/main.cpp
+++ arduino_weather.VIEW/main.cpp
@@ -19,26 +19,13 @@
 #define HUM_KEY "H" /* H - Humidity */
 #define CLEAR_KEY "C" /* C - LCD Clear */
 
-
 int main()
-{
-    WeatherController weatherController;
-    Weather weather = weatherController.get_weather(CITY);
-
-    if (weather.is_empty())
-    {
-        std::cout << "===============ERROR===============\n";
-        std::cout << "something went wrong, the weather is empty\n";
-        return -1;
-    }
-
-#if DEBUG_MODE
-    std::cout << "current temp in " << weather.get_city() << " " << weather.get_temperature() << "C" << '\n';
-    std::cout << "current humidity in " << weather.get_city() << " " << weather.get_humidity() << "%" << '\n';
-#endif
-
+{ 
     try
     {
+        Weather weather;
+        WeatherController weatherController;
+
         boost::asio::io_service io;
         boost::asio::serial_port serial_port(io, PORT);
         
@@ -46,16 +33,49 @@ int main()
         serial_port.set_option(boost::asio::serial_port_base::character_size(CHARACTER_SIZE));
         serial_port.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
 
-        boost::asio::write(serial_port, boost::asio::buffer(CLEAR_KEY));
-        std::this_thread::sleep_for(std::chrono::milliseconds(100 * 10));
+        std::string temp;
+        std::string hum;
+       
+    #if DEBUG_MODE
+        unsigned int counter = 0;
+    #endif
 
-        std::string temp = TEMP_KEY + std::to_string(weather.get_temperature());
-        boost::asio::write(serial_port, boost::asio::buffer(temp));
+        for (;;)
+        {
+            using namespace std::chrono_literals;
 
-        std::string hum = HUM_KEY + std::to_string(weather.get_humidity());
-        boost::asio::write(serial_port, boost::asio::buffer(hum));
+            weather = weatherController.get_weather(CITY);
+            if (weather.is_empty())
+            {
+                std::cout << "===============ERROR===============\n";
+                std::cout << "something went wrong, the weather is empty\n";
+                return -1;
+            }
 
-        serial_port.close();
+            boost::asio::write(serial_port, boost::asio::buffer(CLEAR_KEY));
+            std::this_thread::sleep_for(1s);
+
+            temp = TEMP_KEY + std::to_string(weather.get_temperature());
+            boost::asio::write(serial_port, boost::asio::buffer(temp));
+
+            hum = HUM_KEY + std::to_string(weather.get_humidity());
+            boost::asio::write(serial_port, boost::asio::buffer(hum));
+
+        #if DEBUG_MODE
+            std::cout << "Received data from server --->\n";
+            std::cout << "current temp in " << weather.get_city() << " " << weather.get_temperature() << "C" << '\n';
+            std::cout << "current humidity in " << weather.get_city() << " " << weather.get_humidity() << "%" << "\n\n";
+
+            std::cout << "Data in variables ---> \n";
+            std::cout << "temp: " << temp << '\n';
+            std::cout << "hum: " << hum << '\n';
+            std::cout << "city: " << CITY << '\n';
+
+            std::cout << "Data sending counter: " << ++counter << "\n\n";
+        #endif
+
+            std::this_thread::sleep_for(10min);
+        }
     }
     catch (const std::exception& ex)
     {
